a1a1130395ed868f2a4b38bcf4ca37979f034b2a
Devan <10872224@uvu.edu>
2024-04-02 20:34:19 -0600

    Added Fetch

---
+++ Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float spawnDelay = 1;
+    private float firstDelay = 0;
+    public GameObject dogPrefab;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > firstDelay)
+        {
+            firstDelay = Time.time + spawnDelay;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
